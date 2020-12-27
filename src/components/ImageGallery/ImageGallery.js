71af4a2ea4d8994f2df6e4c836182f5f39840611
Denis Onyshchuk <o.82.denis@gmail.com>
2020-12-27 23:20:16 +0200

    delete comments

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -27,30 +27,22 @@ class ImageGallery extends Component {
     }
 
     componentDidMount() { 
-        console.log("Я Смотнтировался !!!!    - ImageGallery");
-        // this.setState({scrollHeight:  document.documentElement.scrollHeight});  
         this.fetchImagesOnSearch(this.props.searchQuery);
-        // console.log('строка после "this.fetchImagesOnSearch(this.props.searchQuery)"');  
     }
 
     componentDidUpdate(prevProps, prevState) {  
-
-        console.log("ImageGallery.componentDidUpdate", Date.now());
         const prevSearchQuery = prevProps.searchQuery;
         const prevGalleryLength = prevState.gallery.length;
         const stateGalleryLength = this.state.gallery.length;
         const isScroll = (prevGalleryLength*stateGalleryLength);
 
-        // обновилась строка поиска
-        if (this.props.searchQuery !== prevSearchQuery) {
-            // console.log("обновилась строка поиска");
-            // console.log("gallery: []");
+
+        if (this.props.searchQuery !== prevSearchQuery) {         // обновилась строка поиска
             this.setState({ gallery: [] });            
 
             this.fetchImagesOnSearch(this.props.searchQuery);
         }        
-        else if (stateGalleryLength !== prevGalleryLength) {
-            // console.log(stateGalleryLength+"!=="+prevGalleryLength);       
+        else if (stateGalleryLength !== prevGalleryLength) {     
             if(isScroll) window.scrollTo({
                 top: this.scroll,
                 behavior: "smooth",
@@ -63,24 +55,18 @@ class ImageGallery extends Component {
 
     async fetchImagesOnSearch(searchQuery) {
         this.setState({ status: Status.PENDING });  
-
-          console.log('фетчим запрос : ', this.props.searchQuery);
+        //   console.log('фетчим запрос : ', this.props.searchQuery);
 
         const justFetchedImages = await this.apiService.getImages(searchQuery)
             .then(images => images);      
-        
-        //    console.log('пришли с бєкЄнда', justFetchedImages);
 
         //если бекэндотдал пустой массив
         if (!justFetchedImages.length) {
-
-            console.log(" бекэндотдал пустой массив")
-
             this.setState({ status: Status.REJECTED });
             return;
         }
 
-        console.log(" бекэндотдал НЕ пустой массив");
+        // console.log(" бекэндотдал НЕ пустой массив");
         this.setState(({ gallery }) => { 
             const fetchedImages = [...gallery, ...justFetchedImages];
             return { gallery: fetchedImages };
@@ -88,8 +74,7 @@ class ImageGallery extends Component {
 
         this.setState({ status: Status.RESOLVED });
 
-        // console.log('в стейте', this.state.fetchedImages);
-        console.dir(this.apiService.options);
+        // console.dir(this.apiService.options);
     }
 
     loadMore = () => {
@@ -105,7 +90,7 @@ class ImageGallery extends Component {
                     (Status.PENDING === status) && <Loader type='Rings' color="#bfbfbf" className="loader"/>
                 }
                 {
-                    (Status.REJECTED === status) &&  (!gallery.length) && (<h1>Ничего не найдено</h1>)
+                    (Status.REJECTED === status) &&  (!gallery.length) && (<h1>Nothing founded</h1>)
                 }
                 {
                     (!gallery.length)
@@ -124,7 +109,7 @@ class ImageGallery extends Component {
                         
                         {(Status.RESOLVED === status) && < LoadMore loadMore={this.loadMore} />}
 
-                        {(Status.REJECTED === status) &&  <h1>Картинки закончились</h1>}
+                        {(Status.REJECTED === status) &&  <h1>there are no more images....</h1>}
                         
                         
                     </>
